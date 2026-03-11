c04e474d2a6e6f36980e1ff1c0381d23e148fb51
MBSkipper <trawler22@gmail.com>
2026-03-11 18:28:24 +0000

    basic form set up

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
+import {Container} from 'react-bootstrap';  
+
+import Users from './components/Users';
+
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Container className='mt-5'>
+      <Users />
+    </Container>
   )
 }
 
 export default App
+
---
+++ Client/src/components/users.jsx
@@ -0,0 +1,18 @@
+import ListGroup from 'react-bootstrap/ListGroup';
+
+function Users() {
+  return (
+    <>
+      <h1 className='display-5'>Users</h1>
+      <ListGroup>
+        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+        <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+      </ListGroup>
+    </>
+  );
+}
+
+export default Users;
