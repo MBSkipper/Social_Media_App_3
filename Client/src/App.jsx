8cbf3140f150d3973a20d57c5cfa0e6073cc7146
MBSkipper <trawler22@gmail.com>
2026-03-19 12:24:33 +0000

    amend to import file route minor error

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,8 +3,8 @@ import './App.css'
 
 import { Container } from 'react-bootstrap';  
 
-import Users from './components/Users.jsx';
-import AddUser from './components/AddUser.jsx';
+import Users from './components/Users';
+import AddUser from './components/AddUser';
 
 function App() {
 
